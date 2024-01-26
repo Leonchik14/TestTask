e04841c460f2e0c4ae64c5e643edfaa7380b3411
leonchik (Леонид) <leon.ru123@gmail.com>
2024-01-26 10:36:07 +0300

    smart pointers, openMP added

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
+#include <memory>
 #include "Curve.h"
 #include "Circle.cpp"
 #include "Ellipse.cpp"
 #include "Helix.cpp"
 #include "random"
 #include "algorithm"
+#include "omp.h"
 
-std::vector<Curve*> generateFiguresVector(int count, double lowerBound, double upperBound) {
-    std::vector<Curve*> figures(10);
+std::vector<std::shared_ptr<Curve>> generateFiguresVector(int count, double lowerBound, double upperBound) {
+    std::vector<std::shared_ptr<Curve>> figures(count);
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_real_distribution<double> distribution(lowerBound, upperBound);
     for (int i = 0; i < count; ++i) {
         switch (  (int)distribution(rng) % 3) {
             case 0:
-                figures[i] = new Circle(distribution(rng));
+                figures[i] = std::make_shared<Circle>(distribution(rng));
                 break;
             case 1:
-                figures[i] = new Ellipse(distribution(rng), distribution(rng));
+                figures[i] = std::make_shared<Ellipse>(distribution(rng), distribution(rng));
                 break;
             case 2:
-                figures[i] = new Helix(distribution(rng), distribution(rng));
+                figures[i] = std::make_shared<Helix>(distribution(rng), distribution(rng));
                 break;
         }
     }
     return figures;
 }
 
-void printInfoAboutFigures(std::vector<Curve*>& figures, double t) {
+void printInfoAboutFigures(std::vector<std::shared_ptr<Curve>>& figures, double t) {
     for (int i = 0; i < figures.size(); ++i) {
         std::pair <std::vector<double>, std::vector<double>> info = figures[i]->getPointAndDerivative(t);
         std::cout << "Figure " << i + 1 << '\n';
@@ -37,45 +39,46 @@ void printInfoAboutFigures(std::vector<Curve*>& figures, double t) {
     }
 }
 
-std::vector<Circle*> getCirclesFromFigures(std::vector<Curve*>& figures) {
-    std::vector<Circle*> circles;
+std::vector<std::shared_ptr<Circle>> getCirclesFromFigures(std::vector<std::shared_ptr<Curve>>& figures) {
+    std::vector<std::shared_ptr<Circle>> circles;
     for (auto & figure : figures) {
-        if (auto *p = dynamic_cast<Circle *>(figure)) {
+        if (std::shared_ptr<Circle> p = std::dynamic_pointer_cast<Circle>(figure)) {
             circles.push_back(p);
         }
     }
     return circles;
 }
 
-bool circleComparator(const Circle* lhs, const Circle* rhs)
+bool circleComparator(const std::shared_ptr<Circle>& lhs, const std::shared_ptr<Circle>& rhs)
 {
     return lhs->getRadius() < rhs->getRadius();
 }
 
-double getRadiiSum(std::vector<Circle*>& circles) {
-    double  sum = 0;
-    for (auto *circle : circles) {
-        sum += circle->getRadius();
+double getRadiiSum(std::vector<std::shared_ptr<Circle>>& circles) {
+    double radiiSum = 0;
+
+    #pragma omp parallel for reduction(+:radiiSum)
+    for (const auto& circle : circles) {
+        radiiSum += circle->getRadius();
     }
-    return sum;
+    return radiiSum;
 }
 
-void printCirclesRadios(std::vector<Circle*>& circles) {
-    for (auto *circle : circles) {
+void printCirclesRadios(std::vector<std::shared_ptr<Circle>>& circles) {
+    for (const auto& circle : circles) {
         std::cout << circle->getRadius() << " ";
     }
     std::cout << '\n';
 }
 
 int main() {
-    std::vector<Curve*> figures = generateFiguresVector(10, 1, 10);
+    std::vector<std::shared_ptr<Curve>> figures = generateFiguresVector(10000, 1, 10);
     printInfoAboutFigures(figures, M_PI/4);
-    std::vector<Circle*> circles = getCirclesFromFigures(figures);
+    std::vector<std::shared_ptr<Circle>> circles = getCirclesFromFigures(figures);
     std::cout << "Circles before sorting:\n";
     printCirclesRadios(circles);
     std::sort(circles.begin(), circles.end(), circleComparator);
     std::cout << "Circles after sorting:\n";
     printCirclesRadios(circles);
     std::cout << "Sum of all radios: " << getRadiiSum(circles);
-
 }
